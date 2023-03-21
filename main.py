69903b697abb4378d8eecf29042901e172b0b152
fastoch <fastoch@ik.me>
2023-03-21 11:18:50 +0100

    starting to code

---
+++ main.py
@@ -0,0 +1,4 @@
+# First user inputs = deposit + bet
+def deposit():
+    while True:     # I'll keep asking the user to enter a deposit amount until he gives me a valid amount 
+        input = ("")
