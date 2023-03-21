05c065d2debb2989cc201784b4b5bb6046e2e4f0
fastoch <fastoch@ik.me>
2023-03-21 14:21:45 +0100

    deposit function

--- main.py
+++ main.py
@@ -3,4 +3,13 @@ def deposit():
     while True:     # keep asking the user to enter a deposit amount until he gives a valid amount 
         amount = input("How much would you like to deposit? $")
         # I must ensure the user input is valid
-        
+        if amount.isdigit():    # check if inpût is a whole number
+            amount = int(amount)    # user input is always a string, so I have to convert it
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than zero.")
+        else:
+            raise Exception("Please enter a positive number!")
+    return amount
+
